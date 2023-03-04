994668062e227980042527924ceff8b8cabea191
susanthpatnaik <123810843+susanthpatnaik@users.noreply.github.com>
2023-03-04 11:27:20 +0000

    delegates 1st commit

---
+++ HelloWorldCodeSpaces/DelegatesLamdas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace DelegatesLamdas
+{
+    class CheckingDelegates
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WOrld");
+            MathOp MathOperation = sub;
+
+            CalculatePrint(5,6,MathOperation) ;
+        }
+
+        static void CalculatePrint(int x, int y, MathOp f)
+        {
+            Console.WriteLine(f(x,y));
+        }
+
+
+
+        public static int add(int a, int b)
+        {
+            return a+b;
+        }
+
+        public static int sub(int a, int b)
+        {
+            return a-b;
+        }
+
+        delegate int MathOp(int x, int y);
+    }
+
+    
+
+}
