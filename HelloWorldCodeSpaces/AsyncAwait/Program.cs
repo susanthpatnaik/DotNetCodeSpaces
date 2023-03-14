ff5f4eba9ee2d8945be95a781de0dcdd218a3132
susanthpatnaik <123810843+susanthpatnaik@users.noreply.github.com>
2023-03-14 06:33:48 +0000

    generics added

---
+++ HelloWorldCodeSpaces/AsyncAwait/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace asyncawait
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello World");
+            Task.Run(longFunction);
+            shortFunction();
+        }
+
+
+        public static void longFunction()
+        {            
+            
+            Console.WriteLine("Long function started");
+            Thread.Sleep(5000);
+            Console.WriteLine("Long function completed");      
+        }
+
+        public static void shortFunction()
+        {
+            Console.WriteLine("Short function started");
+            Thread.Sleep(1000);
+            Console.WriteLine("Short function completed");
+        }
+    }
+}
---
+++ HelloWorldCodeSpaces/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Generics
+{
+
+    class genericsClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DataStore<string>.printMethod("Hello"));
+            nonGenericClass.print<string>("Hello");
+        }
+    }
+
+    public class DataStore<generic>
+    {
+        public generic Data{get;set;}
+
+        public static generic printMethod(generic data)
+        {
+            return data;
+        }
+    }
+
+    //Class is not generic but method is a generic method
+
+    public class nonGenericClass
+    {
+        public static void print<T>(T data )
+        {
+            Console.WriteLine(data);
+
+        }
+    }
+}
